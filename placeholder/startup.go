49e9615a20d0d7e1f5a3700c5a7bf488350bf30d
Andrey Sokolov <falkonirk@gmail.com>
2024-02-02 14:19:22 +0100

    add authentication to system

--- placeholder/startup.go
+++ placeholder/startup.go
@@ -2,6 +2,7 @@ package placeholder
 
 import (
 	"sync"
+	"webPlatform/authorization"
 	"webPlatform/http"
 	"webPlatform/http/handling"
 	"webPlatform/pipeline"
@@ -18,15 +19,24 @@ func createPipeline() pipeline.RequestPipeline {
 		&basic.StaticFileComponent{},
 		&sessions.SessionComponent{},
 		//&SimpleMessageComponent{},
+		authorization.NewAuthComponent(
+			"protected",
+			authorization.NewRoleCondition("Administrator"),
+			CounterHandler{},
+		),
 		handling.NewRouter(
 			handling.HandlerEntry{"", NameHandler{}},
 			handling.HandlerEntry{"", DayHandler{}},
-			handling.HandlerEntry{"", CounterHandler{}},
+			//handling.HandlerEntry{"", CounterHandler{}},
+			handling.HandlerEntry{"", AuthenticationHandler{}},
 		).AddMethodAlias("/", NameHandler.GetNames),
 	)
 }
 func Start() {
 	sessions.RegisterSessionService()
+	authorization.RegisterDefaultSignInService()
+	authorization.RegisterDefaultUserService()
+	RegisterPlaceholderUserStore()
 	results, err := services.Call(http.Serve, createPipeline())
 	if err == nil {
 		(results[0].(*sync.WaitGroup)).Wait()
